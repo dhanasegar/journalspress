a20a5102b2c78d2ffa9e0398d41e1881c3ff141d
dhanasegar <dhanasegarbit@gmail.com>
2025-04-15 03:13:10 +0530

    host

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j$y6%+jw$mc#mr&5+2cqv^-8q!a#fu7qhj5x-$uagsgp@z%u$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh',"127.0.0.1"]
 
 
 # Application definition
